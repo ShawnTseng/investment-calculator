5af49b205f9801a67b2a6874dbfe47448ed92649
Shawn Tseng <shawntseng40@gmail.com>
2023-03-23 07:18:48 +0800

    change" adjust layout, add best bet ratio input

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatCardModule, MatIconModule, MatInputModule],
+  imports: [CommonModule, FormsModule, MatCardModule, MatGridListModule, MatIconModule, MatInputModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
@@ -19,7 +20,13 @@ export class HomePageComponent {
 
   profitLossRate = 2;
 
+  bestBetRatio = 0;
+
   onWinRateChange(e: any) {
     this.winRate = e.target.value / 100;
+  }
+
+  onCalculateBestBetRatio() {
+
   }
 }
